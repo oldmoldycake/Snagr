b28ac3429cdfdd426d3479c37239ea10b115d9a6
OldMoldyCake <nolan.ruger@gmail.com>
2026-08-08 09:25:08 -0500

    added searc temrs

--- agent/pricing.py
+++ agent/pricing.py
@@ -2,16 +2,26 @@ import requests
 import json
 import logging
 import asyncio
+import os
 from database import get_watched_item_list
 
+SEARCH_URL = os.getenv("SEAR_XNG_URL") 
 log = logging.getLogger(__name__)
-async def search_sear_xng(query, base_url = "http://localhost:8888"):
+queries = ["What is a {} worth", "How much does {} sell for", "{} estimated value" ]
+
+
+
+async def search_sear_xng_queries(base_url = "http://localhost:8888"):
     try:
-        logging.info("Searching SearXNG.....")
-        params = {"q": query, "format": json}
-        response = requests.get(f"{base_url}/search", params=params, timeout=10)
-        response.raise_for_status()
-        return response.json
+        searches = []
+        for query in queries:
+            logging.info("Searching SearXNG.....")
+            params = {"q": query, "format": json}
+            response = requests.get(f"{base_url}/search", params=params, timeout=10)
+            response.raise_for_status()
+            print(response.json)
+            searches.append(response.json)
+        return searches
     except Exception as e:
         logging.error(f"Error searching SearXNG: {e}")
 
@@ -23,6 +33,6 @@ async def parse_sear_xng_results(sear_xng_results):
 
 async def main():
     await get_watched_item_list()
-
+    searches = await search_sear_xng_queries(SEARCH_URL)
 if __name__ == "__main__":
     asyncio.run(main())
